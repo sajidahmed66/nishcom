0e769f09a324f64b25cc3d62bf33125f0bda1254
sajidahmed66 <sajidahmedsiddiqui66@gmail.com>
2022-01-21 23:56:24 +0600

    project init

---
+++ src/components/Main.tsx
@@ -0,0 +1,8 @@
+import Home from './homeComponents/Home';
+const Main = () => {
+    return (
+        <Home />
+    )
+};
+
+export default Main;
---
+++ src/components/homeComponents/Home.tsx
@@ -0,0 +1,11 @@
+import Menu from '../Menu';
+
+const Home = () => {
+    return (
+        <>
+            <Menu />
+        </>
+    )
+}
+
+export default Home;
